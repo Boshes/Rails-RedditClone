f40f1e246539061e706a1c139af3a3d29572d430
Justen Morgan <justenamorgan@gmail.com>
2015-04-21 09:26:46 -0700

    Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
   resources :links do
     member do
       get "like", to: "links#upvote"
       get "dislike", to: "links#downvote"
     end
+    resources :comments
   end
-
 
   root  "links#index"
 
